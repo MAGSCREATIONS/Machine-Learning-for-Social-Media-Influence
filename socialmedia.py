38f8dad5a75d53119aab5b34026331480fd9b94f
kiarajain32-coder <kiara.jain32@gmail.com>
2026-05-29 16:53:54 +0530

    socialmedia.py
    added data type selection.

--- socialmedia.py
+++ socialmedia.py
@@ -10,12 +10,5 @@ print(df.info())
 print(df.describe())
 print(df.isnull().sum())
 print(df.duplicated().sum())
-
-quality_counts = df['quality'].value_counts()
-
-plt.figure(figsize=(8, 6))
-plt.bar(quality_counts.index, quality_counts, color='deeppink')
-plt.title('Count Plot of Quality')
-plt.xlabel('Quality')
-plt.ylabel('Count')
-plt.show()
+df.select_dtypes(include='object').columns
+df.select_dtypes(exclude='object').columns
